e9f42ddc61c5d002f02823432f120a90c28232f9
Adouken133 <55823258+Adouken133@users.noreply.github.com>
2019-11-30 20:34:18 +0800

    Add files via upload

---
+++ final project.py
@@ -0,0 +1,118 @@
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import Imputer
+from sklearn.decomposition import PCA
+
+np.warnings.filterwarnings('ignore')
+
+vgsale = pd.read_csv('vgsales.csv')
+
+
+# Transforming the target information in numerical information
+Tf = preprocessing.LabelEncoder()
+new_Genre = Tf.fit_transform(vgsale['Genre'])
+vgsale['Genre_new'] = new_Genre
+
+#print(vgsale.corr().to_string())
+
+
+# Encoding categorical variables using dummy variables
+encoded_Publisher = pd.get_dummies(vgsale['Publisher'], drop_first=True)
+encoded_Platform = pd.get_dummies(vgsale['Platform'], drop_first=True)
+vgsale = pd.concat([vgsale, encoded_Publisher, encoded_Platform], axis=1)
+
+# Drop useless Column
+vgsale.drop(['Name', 'Genre', 'Platform', 'Publisher'], axis=1, inplace=True)
+
+
+#fill Nan and infinte values
+imputer = Imputer(strategy='median')
+FN = imputer.fit_transform(vgsale)
+vgsale = pd.DataFrame(FN, columns=vgsale.columns)
+
+
+
+
+
+
+
+#Apply machine model
+X = vgsale.drop('Genre_new', axis=1)
+y = vgsale['Genre_new']
+
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.35)
+
+
+
+
+from sklearn.linear_model import LogisticRegression
+lr = LogisticRegression()
+lr.fit(X_train, y_train)
+
+predicted = lr.predict(X_test)
+
+#test model
+# Printing the classification report
+from sklearn.metrics import classification_report, confusion_matrix, f1_score
+#print('Classification Report')
+#print(classification_report(y_test, predicted))
+
+# Printing the classification confusion matrix (diagonal is true)
+#print('Confusion Matrix')
+#print(confusion_matrix(y_test, predicted))
+
+#print('Overall f1-score')
+#print(f1_score(y_test, predicted, average="macro"))
+
+
+# Visualizing structure of dataset in 2D
+pca = PCA(n_components=2)
+proj = pca.fit_transform(X)
+plt.figure(figsize=(20,20))
+plt.scatter(proj[:, 0], proj[:, 1], c=y, edgecolors='black')
+plt.colorbar()
+plt.show()
+
+# This time we plot multiple plots on the same axes, to get some perspective on their comparisons
+plt.figure(figsize=(10,10))
+plt.scatter(vgsale['Genre_new'], vgsale['Global_Sales'], alpha=0.7, label='Global_Sales')
+plt.scatter(vgsale['Genre_new'], vgsale['JP_Sales'], alpha=0.7, label='JP_Sales')
+plt.scatter(vgsale['Genre_new'], vgsale['NA_Sales'], alpha=0.7, label='NA_Sales')
+plt.xlabel('Genre_new')
+plt.ylabel('Global_Sales / JP_Sales / Global_Sales')
+plt.title(f'Genre_new comparisons')
+plt.legend()
+plt.show()
+
+# This time we plot multiple plots on the same axes, to get some perspective on their comparisons
+# The size parameter can be either a fixed value or another columns as in here
+plt.figure(figsize=(10,10))
+plt.scatter(vgsale['Genre_new'], vgsale['Global_Sales'], s=(vgsale['JP_Sales']) ** 2.5,
+             label=f'Genre_new to Global_Sales', color='orange', marker='o', edgecolors='w', alpha=0.7)
+
+plt.xlabel(f'Genre_new')
+plt.ylabel(f'Global_Sales')
+plt.title(f'Alcohol, Malic Acid and Color Intensity (Size)')
+
+plt.legend()
+plt.show()
+
+plt.close()
+
+
+
+
+
+
+
+
+
+
+
+
